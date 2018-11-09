3531282311ad8d4a51cdbe633a55cdd2cf18c903
kingtarun <tkandala@gmail.com>
2018-11-09 14:08:38 -0500

    Assignment 1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import Dishdetail from './DishdetailComponent';
 
 class Menu extends Component {
 
@@ -18,22 +19,9 @@ class Menu extends Component {
   }
 
   renderDish(dish){
-  	if (dish != null){
-  		return (
-  			<Card>
-  				<CardImg width="100%" src={dish.image} alt={dish.name} />
-  				<CardBody>
-  					<CardTitle>{dish.name}</CardTitle>
-  					<CardText>{dish.description}</CardText>
-  				</CardBody>
-  			</Card>
+  	return (
+  			<Dishdetail dish={dish} />
   		);
-  	}
-  	else {
-  		return (
-  			<div></div>
-  		);
-  	}
   }
 
 	render(){
