3e096789603b2fd2e1da59d7696dd7dab7b8c56a
kingtarun <tkandala@gmail.com>
2019-04-23 16:08:27 -0400

    Assignment 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,18 +7,52 @@ class Dishdetail extends Component {
 		super(props);
 	}
 
+	renderDish(dish){
+		return(
+			<Card>
+				<CardImg width="100%" src={dish.image} alt={dish.name} />
+				<CardBody>
+					<CardTitle>{dish.name}</CardTitle>
+					<CardText>{dish.description}</CardText>
+				</CardBody>
+			</Card>
+		);
+	}
+
+	renderComments(comments){
+		if (comments != null){
+			this.comment_list = comments.map((comment,key) => 
+				<li>
+					{comment.comment}<br/>
+					-- {comment.author}, {comment.date}
+				</li>
+			);
+			return(
+				<ul className="list-unstyled">
+					{this.comment_list}
+				</ul>
+			);
+		} else {
+			return(
+				<div></div>
+			)
+		}
+	}
+
 	render(){
 		const dish = this.props.dish;
 		if (dish != null){
 	  		return (
 	  			<div className="container">
-	  			<Card>
-	  				<CardImg width="100%" src={dish.image} alt={dish.name} />
-	  				<CardBody>
-	  					<CardTitle>{dish.name}</CardTitle>
-	  					<CardText>{dish.description}</CardText>
-	  				</CardBody>
-	  			</Card>
+					<div className="row">
+						<div className="col-12 col-md-5 m-1">
+							{this.renderDish(dish)}
+						</div>
+						<div className="col-12 col-md-5 m-1">
+							<h4>Comments</h4>	
+							{this.renderComments(dish.comments)}
+						</div>
+					</div>
 	  			</div>
 	  		);
 	  	}
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -8,14 +8,13 @@ class Menu extends Component {
 	}
 
 	render(){
-
 		const menu = this.props.dishes.map((dish) => {
 			return (
 				<div key={dish.id} className="col-12 col-md-5 m-1">
 					<Card onClick={() => this.props.onClick(dish.id)}>
 						<CardImg width="100%" src={dish.image} alt={dish.name} />
 						<CardImgOverlay>
-              <CardTitle>{dish.name}</CardTitle>
+              				<CardTitle>{dish.name}</CardTitle>
 						</CardImgOverlay>
 					</Card>
 				</div>
@@ -25,7 +24,7 @@ class Menu extends Component {
 		return (
 			<div className="container">
 				<div className="row">
-						{menu}
+					{menu}
 				</div>
 			</div>
 		);
